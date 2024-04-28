4b088d48e00b8dc18b3fe6f962053092b68f0040
Deyb <112176170+dackydeyb@users.noreply.github.com>
2024-04-28 08:27:54 +0800

    changed comments

--- mainsolverplus.java
+++ mainsolverplus.java
@@ -223,7 +223,7 @@ class OneAngleOneSide { // Contains all the methods for solving a right triangle
         File file = new File("One Side One Angle.txt");
 
         // When writing to a file, often wrap PrintWriter around a FileWriter to handle file creation and character encoding
-        // The second argument true in FileWriter constructor is for enabling the append mode, which allows you to add to the existing content of the file instead of overwriting it.
+        // The second argument true in FileWriter constructor is for enabling the append mode, which allows to add to the existing content of the file instead of overwriting it.
         PrintWriter writer = new PrintWriter(new FileWriter(file, true));
         writer.println("Hypotenuse: " + sideC);
         writer.println("Adjacent: " + sideB);
